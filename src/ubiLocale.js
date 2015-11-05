5a23d8ac4badab6693921b147172aa4ddd6218f4
ubi <yourowndisaster0@gmail.com>
2015-11-06 01:13:34 +0800

    Entire script

---
+++ src/ubiLocale.js
@@ -0,0 +1,62 @@
+;(function(angular) {
+  "use strict";
+
+  var count = 1, names = [];
+  var moduleFn = angular.module;
+  angular.module = function(name, requires, configFn) {
+    if (name === "ngLocale") {
+      var uniqueName = "ngLocale" + count;
+      names.push(uniqueName);
+      count = count + 1;
+      return moduleFn.call(this, uniqueName, requires, configFn);
+    } else {
+      return moduleFn.apply(this, arguments);
+    }
+  };
+
+  angular.module("ubiLocale", []);
+
+  angular.module("ubiLocale").provider("$dynamicLocale", [
+    function() {
+      var localesCache = {};
+
+      this.register = function register(id, rules) {
+        localesCache[id] = rules;
+      };
+
+      this.$get = [
+        "$locale",
+        function($locale) {
+          var extend = (function ext(dest, src) {
+            for (var k in dest) {
+              if (angular.isObject(dest[k])) {
+                ext(dest[k], src[k]);
+              } else {
+                dest[k] = src[k];
+              }
+            }
+          });
+
+          return {
+            set: function(id) {
+              extend($locale, localesCache[id]);
+            }
+          };
+        }
+      ];
+    }
+  ]);
+
+  angular.module("ubiLocale").config([
+    "$dynamicLocaleProvider",
+    function($dynamicLocaleProvider) {
+      names.forEach(function(name) {
+        var $injector = angular.injector([name]);
+        var $locale = $injector.get("$locale");
+        $dynamicLocaleProvider.register($locale.id, $locale);
+      });
+    }
+  ]);
+
+})(angular);
+
